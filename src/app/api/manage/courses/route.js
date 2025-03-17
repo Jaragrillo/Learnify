728ac7901e4782a472276fa696197b06a3480b69
Jaragrillo <jaramilloecheverri.sebastian.92@gmail.com>
2025-03-17 11:32:17 -0500

    revisar pagos pendiente, dashboard cursos a medias

---
+++ src/app/api/manage/courses/route.js
@@ -0,0 +1,103 @@
+// pages/api/manage/courses.js
+import { Course, Sale, Rating, User, Category } from '@/models/index';
+import { NextResponse } from 'next/server';
+import { Sequelize, Op } from 'sequelize';
+
+export async function GET(req, res) {
+    try {
+        // Cursos más comprados
+        const cursosMasCompradosSales = await Sale.findAll({
+            attributes: ['id_curso', [Sequelize.fn('COUNT', Sequelize.col('Sale.id_curso')), 'ventas']],
+            group: ['id_curso'],
+            order: [[Sequelize.literal('ventas'), 'DESC']],
+            limit: 4,
+        });
+
+        const cursosMasCompradosData = await Promise.all(cursosMasCompradosSales.map(async venta => {
+            const course = await Course.findOne({
+                where: { id_curso: venta.dataValues.id_curso },
+                attributes: ['titulo', 'estudiantes', 'id_autor'],
+                include: [{
+                    model: User,
+                    as: 'autor',
+                    attributes: ['nombre', 'apellidos', 'foto_perfil'],
+                }],
+            });
+
+            if (!course) {
+                console.error("Error: No se encontró el curso con id:", venta.dataValues.id_curso);
+                return null;
+            }
+
+            return {
+                title: course.titulo,
+                students: course.estudiantes,
+                incomes: course.Sales ? course.Sales.reduce((total, sale) => total + sale.precio, 0) : 0,
+                author: {
+                    nombreCompleto: `${course.autor.nombre} ${course.autor.apellidos}`,
+                    profileImage: course.autor.foto_perfil,
+                },
+            };
+        }));
+
+        // Filtra los resultados nulos
+        const filteredCursosMasCompradosData = cursosMasCompradosData.filter(item => item !== null);
+
+        // Cursos mejor valorados
+        const cursosMejorValoradosRatings = await Rating.findAll({
+            attributes: ['id_curso', [Sequelize.fn('AVG', Sequelize.col('Rating.puntuacion')), 'promedio']],
+            group: ['id_curso'],
+            order: [[Sequelize.literal('promedio'), 'DESC']],
+            limit: 4,
+        });
+
+        const cursosMejorValoradosData = await Promise.all(cursosMejorValoradosRatings.map(async rating => {
+            const course = await Course.findOne({
+                where: { id_curso: rating.dataValues.id_curso },
+                attributes: ['titulo', 'descripcion', 'id_autor'],
+                include: [{
+                    model: User,
+                    as: 'autor',
+                    attributes: ['nombre', 'apellidos', 'foto_perfil'],
+                }],
+            });
+
+            if (!course) {
+                console.error("Error: No se encontró el curso con id:", rating.dataValues.id_curso);
+                return null;
+            }
+
+            return {
+                title: course.titulo,
+                description: course.descripcion,
+                rating: rating.dataValues.promedio,
+                author: {
+                    nombreCompleto: `${course.autor.nombre} ${course.autor.apellidos}`,
+                    profileImage: course.autor.foto_perfil,
+                },
+            };
+        }));
+
+        // Filtra los resultados nulos
+        const filteredCursosMejorValoradosData = cursosMejorValoradosData.filter(item => item !== null);
+
+        // Tabla de cursos y categorías (sin cambios)
+        const cursos = await Course.findAll({
+            attributes: ['id_curso', 'titulo', 'precio', 'estudiantes', 'id_autor', 'id_categoria'],
+        });
+
+        const categorias = await Category.findAll({
+            attributes: ['id_categoria', 'categoria', 'descripcion'],
+        });
+
+        return NextResponse.json({
+            cursosMasComprados: filteredCursosMasCompradosData,
+            cursosMejorValorados: filteredCursosMejorValoradosData,
+            cursos,
+            categorias,
+        });
+    } catch (error) {
+        console.error('Error al obtener datos del dashboard de cursos:', error);
+        return NextResponse.json({ message: 'Error al obtener datos del dashboard de cursos' }, { status: 500 });
+    }
+}
--- src/app/manage/courses/page.jsx
+++ src/app/manage/courses/page.jsx
@@ -1,63 +1,193 @@
+'use client'
+
 import Image from "next/image";
+import { useState, useEffect } from 'react';
 
 export default function AdminCoursesPage() {
+  const [dashboardCourseData, setDashboardCourseData] = useState({
+    cursosMasComprados: [],
+    cursosMejorValorados: [],
+    cursos: [],
+    categorias: [],
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+        try {
+            const response = await fetch('/api/manage/courses');
+            if (response.ok) {
+                const data = await response.json();
+                setDashboardCourseData(data);
+            } else {
+                console.error('Error fetching data');
+            }
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    fetchData();
+  }, []);
+
+  // Obtenemos las estrellas de valoración
+  const renderStars = (rating) => {
+    const fullStars = Math.floor(rating) || 0;
+    const emptyStars = 5 - fullStars;
+
     return (
-      <>
-        <main className="ml-80">
-          <section className="pt-10 px-10">
-            <div className="flex items-center gap-2">
-              <Image 
-                  src="/svg/class.svg" 
-                  alt="class-svg" 
-                  width={50} 
-                  height={50} 
-              />
-              <h2 className="text-4xl text-[#0D1D5F]">Cursos</h2>
-            </div>
-          </section>
-          <section className="p-10 relative">
-            <div className="flex justify-between">
-              <div className="w-[30%] border-2 border-[#0D1D5F] rounded-lg p-5">
-                <h3 className="text-xl font-medium">Cursos creados hoy</h3>
-                <p></p>
-              </div>
-              <div className="w-[30%] border-2 border-[#0D1D5F] rounded-lg p-5">
-                <h3 className="text-xl font-medium">Cursos creados en la semana</h3>
-                <p></p>
-              </div>
-              <div className="w-[30%] border-2 border-[#0D1D5F] rounded-lg p-5">
-                <h3 className="text-xl font-medium">Cursos creados en el mes</h3>
-                <p></p>
-              </div>
-            </div>
-          </section>
-          <section className="p-10">
-            <div>
-              <label htmlFor="filterCoursesBy">Filtrar cursos por:</label>
-              <select name="filterCoursesBy" id="filterCoursesBy">
-                <option value="">Cursos</option>
-                <option value="">Cursos creados</option>
-                <option value="">Cursos eliminados</option>
-                <option value="">Cursos más vendidos</option>
-              </select>
-            </div>
-            <div>
-              chart
-            </div>
-          </section>
-          <section>
-            <h3>Cursos más comprados</h3>
-            <div>cursos mas comprados query</div>
-          </section>
-          <section>
-            <h3>Cursos mejor valorados</h3>
-            <div>cursos mejor valorados query</div>
-          </section>
-          <section>
-            <h3>Cursos</h3>
-            <div>tabla cursos</div>
-          </section>
-        </main>
-      </>
+        <div className="flex items-center">
+            {/* Estrellas llenas */}
+            {Array.from({ length: fullStars }, (_, index) => (
+                <Image
+                    key={`full-${index}`}
+                    src="/svg/star.svg"
+                    alt="star-svg"
+                    width={24}
+                    height={24}
+                />
+            ))}
+            {/* Estrellas vacías */}
+            {Array.from({ length: emptyStars }, (_, index) => (
+                <Image
+                    key={`empty-${index}`}
+                    src="/svg/emptyStar.svg"
+                    alt="emptyStar-svg"
+                    width={24}
+                    height={24}
+                />
+            ))}
+        </div>
     );
+  };
+
+  return (
+    <>
+      <main className="ml-80">
+        <section className="pt-10 px-10">
+          <div className="flex items-center gap-2">
+            <Image 
+                src="/svg/class.svg" 
+                alt="class-svg" 
+                width={50} 
+                height={50} 
+            />
+            <h2 className="text-4xl text-[#0D1D5F]">Cursos</h2>
+          </div>
+        </section>
+        <section className="px-10 pt-10">
+          <div>
+            chart cursos más vendidos
+          </div>
+          <div className="my-10 h-[2px] w-full bg-[#0D1D5F]/60 rounded-xl"></div>
+        </section>
+        <section className="px-10">
+          <h3 className="text-3xl text-center text-[#0D1D5F] mb-10">Cursos más comprados</h3>
+          <div className="flex justify-between">
+            {dashboardCourseData.cursosMasComprados.map((course, index) => (
+              <div key={index} className="w-2/5 shadow-lg shadow-black/60 rounded-lg p-5">
+                <h4 className="text-2xl text-[#0D1D5F] mb-3">{course.title}</h4>
+                <div className="flex items-center gap-1 mb-3">
+                  <Image 
+                    src="/svg/student.svg" 
+                    alt="student-svg" 
+                    width={50} 
+                    height={50} 
+                  />
+                  <div>
+                    <h5>Estudiantes actuales</h5>
+                    <p>{course.students}</p>  
+                  </div>  
+                </div>  
+                <div className="flex items-center gap-1 mb-3">
+                  <Image 
+                    src="/svg/incomes.svg" 
+                    alt="incomes-svg" 
+                    width={50} 
+                    height={50} 
+                  />
+                  <div>
+                    <h5>Ganancias generadas</h5>
+                    <p>${course.incomes} COP</p>  
+                  </div>  
+                </div>  
+                <div className="flex items-center gap-1">
+                  <div className="w-[50px] h-[50px] overflow-hidden rounded-full">
+                    <Image 
+                      src={course.author.profileImage} 
+                      alt={`${course.author.nombreCompleto}-picture`} 
+                      width={50} 
+                      height={50} 
+                      className="rounded-full object-cover"
+                    />
+                  </div>
+                  <div>
+                    <h5>Creado por:</h5>
+                    <p>{course.author.nombreCompleto}</p>
+                  </div>  
+                </div>  
+              </div>  
+            ))}            
+          </div>
+          <div className="my-10 h-[2px] w-full bg-[#0D1D5F]/60 rounded-xl"></div>
+        </section>
+        <section className="px-10">
+          <h3 className="text-3xl text-center text-[#0D1D5F] mb-10">Cursos mejor valorados</h3>
+          <div className="flex justify-between">
+            {dashboardCourseData.cursosMejorValorados.map((course, index) => (
+              <div key={index} className="shadow-lg shadow-black/60 rounded-lg p-5 w-full">
+                <h4 className="text-2xl text-[#0D1D5F] mb-3">{course.title}</h4>
+                <div className="flex items-center mb-3">
+                  {renderStars(Math.round(course.rating))} {/* Renderiza las estrellas */}
+                  <p className="ml-2">({Math.round(course.rating)})</p> {/* Muestra el rating redondeado */}
+                </div>
+                <p className="text-justify text-black/60 mb-3">{course.description}</p>
+                <div className="flex items-center">
+                  <div className="w-[50px] h-[50px] overflow-hidden rounded-full"> 
+                    <Image 
+                      src={course.author.profileImage} 
+                      alt={`${course.author.nombreCompleto}-picture`} 
+                      width={50} 
+                      height={50} 
+                    />
+                  </div>
+                  <div>
+                    <h5>Creado por:</h5>
+                    <p>{course.author.nombreCompleto}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="my-10 h-[2px] w-full bg-[#0D1D5F]/60 rounded-xl"></div>
+        </section>
+        <section className="px-10">
+          <h3>Cursos</h3>
+          <div>tabla cursos</div>
+          <div className="my-10 h-[2px] w-full bg-[#0D1D5F]/60 rounded-xl"></div>
+        </section>
+        <section className="px-10">
+          <div className="flex items-center gap-2">
+            <Image 
+                src="/svg/class.svg" 
+                alt="class-svg" 
+                width={50} 
+                height={50} 
+            />
+            <h2 className="text-4xl text-[#0D1D5F]">Cursos</h2>
+          </div>
+          <div>
+            <div>
+              tabla categorias
+            </div>
+            <div>
+              <button>
+                <p>Añadir nueva categoría</p>
+                <Image></Image>
+              </button>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
