2e53947ccace6c60f4467e07c8e14d2db979280c
Jaragrillo <jaramilloecheverri.sebastian.92@gmail.com>
2025-03-25 11:50:21 -0500

    responsive del nav del usuario logeado, del home y cursos hecho

--- src/components/NavbarLoggedIn.jsx
+++ src/components/NavbarLoggedIn.jsx
@@ -14,8 +14,9 @@ export default function NavbarLoggedIn() {
     const [error, setError] = useState(null);
     const router = useRouter();
     const pathname = usePathname();
-    const [searchTerm, setSearchTerm] = useState('');
-    const [searchResults, setSearchResults] = useState([]);
+    const [searchTerm, setSearchTerm] = useState(''); // Estado para manejar el término de busqueda
+    const [searchResults, setSearchResults] = useState([]); // Estado para manejar las respuestas del buscador
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // Estado para manejar el menú móvil
 
     const handleSearch = async (query) => { 
         if (!query.trim()) {
@@ -85,15 +86,20 @@ export default function NavbarLoggedIn() {
         return () => clearTimeout(delaySearch); // Limpia el timeout si el componente se desmonta o searchTerm cambia
     }, [searchTerm]);
 
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
     if (error) {
         return <p className="text-red-500">{error}</p>;
     }
 
     return (
         <>
-            <header className="h-[148px]">
-                <nav className="text-white bg-gradient-to-r from-[#34ADDA] via-30% via-[#1E88C6] to-[#0E4472] flex items-center justify-between fixed w-full z-40">
-                    <div className="block">
+            <header className="h-[100px] sm:h-[148px]">
+                <nav className="text-white bg-gradient-to-r from-[#34ADDA] via-30% via-[#1E88C6] to-[#0E4472] flex items-center justify-between fixed w-full z-40 h-[100px]">
+                    {/* Logo */}
+                    <div className="hidden sm:block">
                         <Link href={"/user/home"}>
                             <Image
                                 src="/images/learnifyLogo.jpeg"
@@ -105,16 +111,24 @@ export default function NavbarLoggedIn() {
                         </Link>
                     </div>
 
-                    <div className='hidden lg:block'>
+                    {/* Input busqueda de cursos */}
+                    <div className='hidden md:block'>
                         <div className='relative'>
                             <input
                                 type="text"
                                 name="searchCourse"
                                 id="searchCourse"
                                 placeholder='¿Qué te intersa aprende hoy?'
-                                className='rounded-lg px-3 py-2 w-[700px] focus:outline-none text-black'
+                                className='rounded-lg px-3 py-2 w-[400px] lg:w-[700px] focus:outline-none text-black'
                                 value={searchTerm}
                                 onChange={(e) => setSearchTerm(e.target.value)}
+                            />
+                            <Image
+                                src="/svg/search.svg"
+                                alt="search-svg"
+                                width={24}
+                                height={24}
+                                className="absolute right-5 top-0 bottom-0 my-auto "
                             />
                             {searchResults.length > 0 ? (
                                 <ul className="absolute bg-white text-black shadow-md shadow-black/50 rounded-md w-full mt-1 max-h-48 overflow-y-auto z-50">
@@ -135,7 +149,26 @@ export default function NavbarLoggedIn() {
                             )}
                         </div>
                     </div>
+                    
+                    {/* Botón para el desplegable */}
+                    <div className="sm:hidden">
+                        <button onClick={toggleMobileMenu} className="p-4">
+                            {isMobileMenuOpen ? (
+                                <svg className="h-12 w-12 fill-current text-white" viewBox="0 0 24 24">
+                                    <path fillRule="evenodd" clipRule="evenodd" d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.829-4.828 4.829a1 1 0 0 1-1.414-1.414l4.829-4.828-4.829-4.828a1 1 0 1 1 1.414-1.414l4.828 4.828 4.829-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.829z" />
+                                </svg>
+                            ) : (
+                                <Image
+                                    src="/svg/menu.svg"
+                                    alt="menu-icon"
+                                    width={50}
+                                    height={50}
+                                />
+                            )}
+                        </button>
+                    </div>
 
+                    {/* Info perfil */}
                     <ul className='flex items-center mr-5'>
                         <li>
                             <Link href={`/user/profile/`}>
@@ -164,6 +197,34 @@ export default function NavbarLoggedIn() {
                         </li>
                     </ul>
                 </nav>
+
+                {/* SubNavbar para el Móvil */}
+                {isMobileMenuOpen && (
+                    <nav className='sm:hidden bg-[#cee4f1] text-black/60 fixed w-full z-30 mt-[100px]'>
+                        <ul className='flex flex-col items-center'>
+                            <li className='p-3'>
+                                <Link href={"/user/home"} className={`hover:text-black/100 ${pathname === '/user/home' ? 'text-black/100' : ''}`}>Home</Link>
+                            </li>
+                            <li className='p-3'>
+                                <Link href={"/user/courses"} className={`hover:text-black/100 ${pathname.includes('courses') ? 'text-black/100' : ''}`}>Cursos</Link>
+                            </li>
+                            <li className='p-3'>
+                                <Link href={"/user/categories"} className={`hover:text-black/100 ${pathname.includes('categories') ? 'text-black/100' : ''}`}>Categorías</Link>
+                            </li>
+                            <li className='p-3'>
+                                <Link href={"/user/purchasedCourses"} className={`hover:text-black/100 ${pathname.includes('purchasedCourses') ? 'text-black/100' : ''}`}>Cursos comprados</Link>
+                            </li>
+                            <li className='p-3'>
+                                <Link href={"/user/myCourses"} className={`hover:text-black/100 ${pathname.includes('myCourses') ? 'text-black/100' : ''}`}>Cursos creados</Link>
+                            </li>
+                            <li className='p-3'>
+                                <Link href={"/user/community/forums"} className={`hover:text-black/100 ${pathname.includes('community') ? 'text-black/100' : ''}`}>Comunidad</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                )}
+
+                {/* SubNavbar para el Escritorio */}
                 <nav className='hidden sm:block bg-[#cee4f1] text-black/60 fixed w-full z-30 mt-[100px]'>
                     <ul className='flex items-center'>
                         <li className='p-3'>
