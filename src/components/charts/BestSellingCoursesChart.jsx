e994996e9c1e0f0e54bc62839dd7e9301a5749db
Jaragrillo <jaramilloecheverri.sebastian.92@gmail.com>
2025-03-26 22:41:42 -0500

    cambio libreria correccion dependencias prueba

--- src/components/charts/BestSellingCoursesChart.jsx
+++ src/components/charts/BestSellingCoursesChart.jsx
@@ -1,47 +1,53 @@
-'use client'
+// 'use client'
 
-import { Bar } from 'react-chartjs-2';
-import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
-import React, { useEffect } from 'react';
+// import { Bar } from 'react-chartjs-2';
+// import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
+// import React, { useEffect } from 'react';
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+// ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
-function BestSellingCoursesChart({ data }) {
-    useEffect(() => {
-        console.log("BestSellingCoursesChart Data:", data);
-    }, [data]);
+// function BestSellingCoursesChart({ data }) {
+//     useEffect(() => {
+//         console.log("BestSellingCoursesChart Data:", data);
+//     }, [data]);
 
-    const chartData = {
-        labels: data.labels,
-        datasets: [{
-            label: 'Ventas',
-            data: data.datasets[0].data,
-            backgroundColor: '#cee4f1',
-            borderColor: '#0D1D5F', 
-            borderWidth: 1, 
-        }],
-    };
+//     const chartData = {
+//         labels: data.labels,
+//         datasets: [{
+//             label: 'Ventas',
+//             data: data.datasets[0].data,
+//             backgroundColor: '#cee4f1',
+//             borderColor: '#0D1D5F', 
+//             borderWidth: 1, 
+//         }],
+//     };
 
-    const options = {
-        responsive: true,
-        maintainAspectRatio: false,
-        redraw: true,
-        plugins: {
-            legend: { position: 'top', labels: { font: { size: 12 } } },
-            title: { display: true, text: 'Cursos Más Vendidos', font: { size: 25, weight: '200' }, color: '#0D1D5F', },
-        },
-        scales: {
-            x: {
-                title: { display: true, text: 'Cursos', font: { size: 20, weight: '600' }, color: '#0D1D5F', },
-            },
-            y: {
-                title: { display: true, text: 'Ventas', font: { size: 20, weight: '600' }, color: '#0D1D5F', },
-                beginAtZero: true, // Inicia el eje Y desde 0
-            },
-        },
-    };
+//     const options = {
+//         responsive: true,
+//         maintainAspectRatio: false,
+//         redraw: true,
+//         plugins: {
+//             legend: { position: 'top', labels: { font: { size: 12 } } },
+//             title: { display: true, text: 'Cursos Más Vendidos', font: { size: 25, weight: '200' }, color: '#0D1D5F', },
+//         },
+//         scales: {
+//             x: {
+//                 title: { display: true, text: 'Cursos', font: { size: 20, weight: '600' }, color: '#0D1D5F', },
+//             },
+//             y: {
+//                 title: { display: true, text: 'Ventas', font: { size: 20, weight: '600' }, color: '#0D1D5F', },
+//                 beginAtZero: true, // Inicia el eje Y desde 0
+//             },
+//         },
+//     };
 
-    return <Bar options={options} data={chartData} />;
+//     return <Bar options={options} data={chartData} />;
+// }
+
+// export default React.memo(BestSellingCoursesChart);
+
+export default function BestSellingCoursesChart() {
+    return(
+        <div>barchart</div>
+    )
 }
-
-export default React.memo(BestSellingCoursesChart);
